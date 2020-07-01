f30e54e16be2dc0a18675d698bc0621193335c60
kriss-clotilde <kriss.clotilde@gmail.com>
2020-07-01 08:16:02 +0200

    init

---
+++ src/ecriture/Ecrire.java
@@ -0,0 +1,93 @@
+package ecriture;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Ecrire implements Runnable {
+	private String path;
+	private Double tempeature;
+	private String str;
+
+	public Ecrire(String path, Double temperature) {
+		this.path = path;
+		this.tempeature = temperature;
+	}
+
+	public boolean fichierExist() {
+
+		if (new File(path).isFile()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	public void acquisition() {
+
+		FileWriter fw;
+		try {
+			fw = new FileWriter(this.path);
+			@SuppressWarnings("resource")
+			BufferedWriter write = new BufferedWriter(fw);
+			write.write("" + str);
+			write.close();
+			fw.close();
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void enregistrement() {
+
+		if (!fichierExist()) {
+
+			try {
+				new File(path).createNewFile();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+
+			}
+		}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Veuillez saisir un a et une acquisition :");
+		str = sc.nextLine();
+		
+
+		while (str.contains("a")) {
+
+			str=str.replace('a', ' ');
+
+			acquisition();
+
+			System.out.println("Veuillez saisir un a et une acqcuisition :");
+			str = sc.nextLine();
+		
+
+		}
+	}
+
+	@Override
+	public void run() {
+
+		// TODO Auto-generated method stub
+		while (true) {
+			try {
+				Thread.sleep(500);
+				enregistrement();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+
+}
---
+++ src/temperature/Main.java
@@ -0,0 +1,34 @@
+package temperature;
+
+import ecriture.Ecrire;
+import lecture.Lire;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Ecrire premierInstEcrire=new Ecrire("D:\\kri\\ecrire.txt",2.4);
+		Lire lire=new Lire("D:\\kri\\ecrire.txt");
+		
+	
+		
+	
+		Thread thread2 =  new Thread(lire) ;
+			Thread creation=new Thread(premierInstEcrire); 
+		
+			creation.start();
+			
+				thread2.start();
+			
+			
+			
+			
+			
+		
+		
+	}
+	
+	
+	
+	
+
+}
